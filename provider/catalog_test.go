6e72a61647566f84f097fa044597400bcea842bc
Kevin Kelley <kelleyk@kelleyk.net>
2018-11-29 12:11:15 -0800

    Move setup/teardown stuff into suite-level functions

--- provider/catalog_test.go
+++ provider/catalog_test.go
@@ -14,24 +14,42 @@ import (
 
 type CatalogTestSuite struct {
 	suite.Suite
+
+	ts  *httptest.Server
+	cat *catalog
+
+	upstreamRequestQty int
 }
 
 func TestCatalogTestSuite(t *testing.T) {
 	suite.Run(t, new(CatalogTestSuite))
 }
 
+func (suite *CatalogTestSuite) SetupTest() {
+	t := suite.T()
+
+	suite.upstreamRequestQty = 0
+	suite.ts = httptest.NewServer(http.HandlerFunc(suite.handleUpstreamRequest))
+
+	var err error
+	suite.cat, err = newCatalog(suite.ts.URL)
+	if err != nil {
+		t.Fatalf("failed to create catalog: %v", err)
+	}
+}
+
+func (suite *CatalogTestSuite) TearDownTest() {
+	suite.ts.Close()
+}
+
 func (suite *CatalogTestSuite) handleUpstreamRequest(w http.ResponseWriter, r *http.Request) {
+	suite.upstreamRequestQty++
 	fmt.Fprintln(w, "Hello, client")
 }
 
 func (suite *CatalogTestSuite) TestSimple() {
 	t := suite.T()
-
-	ts := httptest.NewServer(http.HandlerFunc(suite.handleUpstreamRequest))
-	defer ts.Close()
-
-	cat, err := newCatalog(ts.URL)
-	assert.Nil(t, err)
+	cat := suite.cat
 
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
@@ -40,5 +58,7 @@ func (suite *CatalogTestSuite) TestSimple() {
 	assert.Nil(t, err)
 	assert.NotNil(t, m)
 
+	// This should indicate a Content-Length of 14 ("Hello, client\n").
+	// TODO: Replace with an assertion once the test server is serving actual data.
 	fmt.Printf("object metadata: %v\n", m)
 }
