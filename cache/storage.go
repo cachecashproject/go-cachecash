4d1e477c813a436f6af09e60e085f3080a8112d4
Kevin Kelley <kelleyk@kelleyk.net>
2019-01-10 01:59:35 -0800

    Add debug output to CacheStorage

--- cache/storage.go
+++ cache/storage.go
@@ -41,24 +41,44 @@ func (s *CacheStorage) getEscrowState(escrowID uint64) *escrowState {
 // Returns (nil, nil) if the object does not exist; the error part of the rval is reserved for e.g. storage engine
 // errors.
 func (s *CacheStorage) GetMetadata(escrowID, objectID uint64) (*ccmsg.ObjectMetadata, error) {
+	s.l.WithFields(logrus.Fields{
+		"escrowID": escrowID,
+		"objectID": objectID,
+	}).Debug("(*CacheStorage).GetMetadata")
+
 	es := s.getEscrowState(escrowID)
 	m, _ := es.metadata[objectID]
 	return m, nil
 }
 
 func (s *CacheStorage) GetData(escrowID, blockID uint64) ([]byte, error) {
+	s.l.WithFields(logrus.Fields{
+		"escrowID": escrowID,
+		"blockID":  blockID,
+	}).Debug("(*CacheStorage).GetData")
+
 	es := s.getEscrowState(escrowID)
 	b, _ := es.data[blockID]
 	return b, nil
 }
 
 func (s *CacheStorage) PutMetadata(escrowID, objectID uint64, m *ccmsg.ObjectMetadata) error {
+	s.l.WithFields(logrus.Fields{
+		"escrowID": escrowID,
+		"objectID": objectID,
+	}).Debug("(*CacheStorage).PutMetadata")
+
 	es := s.getEscrowState(escrowID)
 	es.metadata[objectID] = m
 	return nil
 }
 
 func (s *CacheStorage) PutData(escrowID, blockID uint64, data []byte) error {
+	s.l.WithFields(logrus.Fields{
+		"escrowID": escrowID,
+		"blockID":  blockID,
+	}).Debug("(*CacheStorage).PutData")
+
 	es := s.getEscrowState(escrowID)
 	es.data[blockID] = data
 	return nil
