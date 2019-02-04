4bb2ca2ad51f6999cc3d2961e7e089b84cc4c6d7
Kevin Kelley <kelleyk@kelleyk.net>
2019-02-04 13:07:03 -0800

    Add simple test program demonstrating database usage

---
+++ cmd/cache-db-test/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/kelleyk/go-cachecash/cache/models"
+	"github.com/kelleyk/go-cachecash/common"
+	"github.com/kelleyk/go-cachecash/testutil"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/volatiletech/sqlboiler/boil"
+)
+
+func main() {
+	ctx := context.Background()
+
+	db, err := sql.Open("sqlite3", "./cache.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lcms, err := models.LogicalCacheMappings().All(ctx, tx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, lcm := range lcms {
+		log.Printf("%v: %v\n", i, lcm)
+	}
+
+	escrowID, err := common.BytesToEscrowID(testutil.RandBytes(common.EscrowIDSize))
+	if err != nil {
+		panic(err)
+	}
+	ne := models.LogicalCacheMapping{
+		EscrowID: escrowID,
+		SlotIdx:  uint64(len(lcms)),
+	}
+	if err := ne.Insert(ctx, tx, boil.Infer()); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("fin")
+	_ = db
+}
