74b4ef33924218b7c6efe71284bf0fecd9146bf0
Kevin Kelley <kelleyk@kelleyk.net>
2019-02-05 13:26:02 -0800

    Keep empty generated database so that sqlboiler can create test databases

--- cache/migrations/gen.go
+++ cache/migrations/gen.go
@@ -3,7 +3,7 @@ package migrations
 //go:generate rm -f cache.db
 //go:generate sql-migrate up -env=cache-development
 //go:generate sqlboiler -o ../models/ sqlite3
-//go:generate rm cache.db
+//go:generate mv cache.db ../models/
 
 //-- go:generate sql-migrate up -env=cache-development-pg
 //-- go:generate sqlboiler -o ../models/ psql
