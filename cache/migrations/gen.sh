6dae7933ce70d436b2a35d96b8918477a731ec52
Kevin Kelley <kelleyk@kelleyk.net>
2019-02-05 14:42:38 -0800

    Add build tag to sqlboiler-generated tests

--- cache/migrations/gen.sh
+++ cache/migrations/gen.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-set -euf -o pipefail
+set -eu -o pipefail
+shopt -s failglob
 
 # It's important to run sqlboiler with `-o .` so that `outputDirDepth` in the generated code is 0.  This makes it look
 # for `sqlboiler.toml` in the same directory as the generated files, which lets us use different configuration files for
 # different sets of models.  Otherwise, it looks only `outputDirDepth` levels above the models (here, it was looking in
 # the repository's root).
 pushd ../models/
-rm -f cache.db
+ rm -f cache.db
 rm -f *.go
 sql-migrate up -config=../migrations/dbconfig.yml -env=cache-development
 sqlboiler -c ../migrations/sqlboiler.toml -o . sqlite3
 popd
+
+# Add build tag to generated tests.
+for SRCFILE in ../models/*_test.go; do
+    ed -s "${SRCFILE}" <<EOF
+1i
+// +build sqlboiler_test
+
+.
+w
+q
+EOF
+
+done
 
 # # For postgres (XXX: Will need paths adjusted)
 # sql-migrate up -env=cache-development-pg
