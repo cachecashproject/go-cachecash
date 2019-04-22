b7a48f1ffd118f47bd8f2339a6739961e50f03b1
cacheflux <markus@cachecash.com>
2019-04-22 22:08:12 +0200

    Update golang in docker tests to 1.12.4

--- ci/setup.sh
+++ ci/setup.sh
@@ -12,25 +12,15 @@ update_docker_compose() {
 
 case "$BUILD_MODE" in
 	test)
-		go get github.com/golangci/golangci-lint/cmd/golangci-lint
-		go get github.com/rubenv/sql-migrate/...
-
-		docker network create cachecash
-		docker run -d -p 5432:5432 -e POSTGRES_DB=publisher --name publisher-db --net=cachecash postgres:10
+		docker network create cachecash || true
+		docker run -d -p 5432:5432 -e POSTGRES_DB=publisher --name publisher-db --net=cachecash postgres:11
 		docker build -t cachecash-ci -f ci/Dockerfile .
 
-		while ! psql 'host=127.0.0.1 port=5432 user=postgres dbname=publisher sslmode=disable' -c 'select 1;'; do sleep 10; done
-		(cd publisher/migrations/; sql-migrate up -config=dbconfig.yml -env=development)
+		# wait until the database is up
+		while ! docker run --rm --net=cachecash cachecash-ci psql 'host=publisher-db port=5432 user=postgres dbname=publisher sslmode=disable' -c 'select 1;'; do sleep 10; done
 
-		# # For once we switch to modules; ensure that golangci-lint is vendored first.
-		# - go install -mod vendor github.com/golangci/golangci-lint/cmd/golangci-lint
-		#
-		# # These are not necessary unless we start doing code generation during CI (which is not a terrible idea; the build
-		# # should fail if any generated code has not been regenerated to reflect changes).
-		# - go get -u github.com/rubenv/sql-migrate/...
-		# - go get -u github.com/volatiletech/sqlboiler/...
-		# - go get -u github.com/volatiletech/sqlboiler-sqlite3/...
-		# - go get -u github.com/volatiletech/sqlboiler/drivers/sqlboiler-psql/...
+		# apply migrations
+		docker run --rm --net=cachecash cachecash-ci sql-migrate up -config=publisher/migrations/dbconfig.yml -env=docker-tests
 		;;
 	docker)
 		update_docker_compose
