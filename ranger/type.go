b5c68f0da0185779b9b8317bc38568e093276208
Robert Collins <robertc@cachecash.com>
2019-11-14 23:06:57 +1300

    Implement Type for ConfigType
    This rounds out delegation of code generation to user defined types.

--- ranger/type.go
+++ ranger/type.go
@@ -74,39 +74,95 @@ func (typ *ConfigType) InterfaceAdapter(instance TypeInstance) TypeInstance {
 }
 
 // Does the type support len()
-func (ct *ConfigType) HasLen(_ TypeInstance) bool {
-	panic("not implemented")
+func (ct *ConfigType) HasLen(instance TypeInstance) bool {
+	return instance.HasLen()
 }
 
 // MinimumSize returns the minimum serialized size of the type.
-func (ct *ConfigType) MinimumSize(_ TypeInstance) uint64 {
-	panic("not implemented")
+func (ct *ConfigType) MinimumSize(instance TypeInstance) uint64 {
+	if ct.IsInterface() {
+		// The switch marker
+		// TODO: switch to the marker + the minimum of the defined cases.
+		return ct.cf.GetType(ct.Interface.Input).MinimumSize(
+			ct.InterfaceAdapter(instance))
+	}
+	var minimum uint64
+	for _, field := range ct.Fields {
+		// roughly:
+		// if structural, can delegate already
+		// if variable, a varint
+		// fixed length is a todo - today its still a varint otherwise
+		instance := field.FieldInstance()
+		field_type := field.GetType()
+		if field_type.HasLen(instance) {
+			// uvarint minimum size, to record a 0 length string/array
+			minimum += 1
+		} else {
+			minimum += field_type.MinimumSize(instance)
+		}
+	}
+	return minimum
 }
 
 // The name of the type
 func (ct *ConfigType) Name() string {
-	panic("not implemented")
+	return ct.TypeName
 }
 
 // PointerType returns whether the type instance is a value or a pointer to
 // value
-func (ct *ConfigType) PointerType(_ TypeInstance) bool {
-	panic("not implemented")
+func (ct *ConfigType) PointerType(instance TypeInstance) bool {
+	return instance.IsPointer()
 }
 
 // Read returns code to deserialise an instance of the type
-func (ct *ConfigType) Read(_ TypeInstance) string {
-	panic("not implemented")
+func (ct *ConfigType) Read(instance TypeInstance) string {
+	if !ct.IsInterface() {
+		return ct.cf.ExecuteString("readstruct.gotmpl", instance)
+	}
+
+	readInput := ct.cf.GetType(ct.Interface.Input).Read(ct.InterfaceAdapter(instance))
+	readStruct := ct.cf.ExecuteString("readstruct.gotmpl", instance)
+
+	return ct.cf.ExecuteString("readinterface.gotmpl", struct {
+		Type       Type
+		Instance   TypeInstance
+		ReadInput  string
+		ReadStruct string
+	}{ct, instance, readInput, readStruct})
+
 }
 
 // WriteSize returns code to caculate the size of an instance of the type when serialized
-func (ct *ConfigType) WriteSize(_ TypeInstance) string {
-	panic("not implemented")
+func (ct *ConfigType) WriteSize(instance TypeInstance) string {
+	if ct.IsInterface() {
+		/* Interface serialialisation:
+		   - the 'input' type, then the delegated case - currently only works with
+			 Minimum / constant size types.
+		*/
+		// instance describes e.g. TransactionBody
+		// interface.input tells us we want TransactionBody.TxType
+		return fmt.Sprintf("%s + %s",
+			ct.cf.GetType(ct.Interface.Input).WriteSize(ct.InterfaceAdapter(instance)),
+			ct.cf.ExecuteString("interfacesize.gotmpl", instance))
+	}
+	return fmt.Sprintf("%s.Size()", instance.WriteSymbolName())
 }
 
 // Write returns code to serialise an instance of the
-func (ct *ConfigType) Write(_ TypeInstance) string {
-	panic("not implemented")
+func (ct *ConfigType) Write(instance TypeInstance) string {
+	if !ct.IsInterface() {
+		/* XXX: Serialising a reference to a user defined type. These are expected
+		   to self-deliimit
+		   - this is one of the points to fix once things are neated up
+		*/
+		return ct.cf.ExecuteString("marshaltostruct.gotmpl", instance)
+	}
+
+	return fmt.Sprintf("%s\n%s",
+		ct.cf.GetType(ct.Interface.Input).Write(ct.InterfaceAdapter(instance)),
+		ct.cf.ExecuteString("marshaltostruct.gotmpl", instance))
+
 }
 
 type InputInstanceAdapter struct {
