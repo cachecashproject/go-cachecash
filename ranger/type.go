262582570c703c97131c11315cbc7270f2197703
Robert Collins <robertc@cachecash.com>
2019-11-14 14:10:04 +1300

    Introduce Type and TypeInstance
    We need a place to abstract out code generation questions for arrays,
    pointers, and symbol names. We also need an abstract interface for
    working with types.
    
    This can be evolved to a more complete type system in future - e.g.
    moving pointer and array knowledge onto a single instance and separating
    out symbol information.

--- ranger/type.go
+++ ranger/type.go
@@ -1,5 +1,54 @@
 package ranger
 
+// TypeInstance is an instance of a type
+// e.g. Type == struct definition.
+//      TypeInstance == usage - plain, in an array, or as a pointer.
+type TypeInstance interface {
+	// Get at the ConfigFormat for e.g. running templates
+	ConfigFormat() *ConfigFormat
+	// What if any Length is configured for 'instance'.
+	// NB: this should move to being part of the type.
+	GetLength() uint64
+	// What max length is configured for 'instance'.
+	// Should fall back to MaxByteRange (though Kevin has asked that that be
+	// removed)
+	GetMaxLength() uint64
+	// Does the type support len()
+	HasLen() bool
+	// Is this a Reference
+	IsPointer() bool
+	// Whats the fully qualified name (for human errors)
+	QualName() string
+	// How is this instance address in code when reading in
+	ReadSymbolName() string
+	// How is this instance addressed in code when writing out
+	WriteSymbolName() string
+	// Is this instance statically sized? (Separate to static array lengths at
+	// least for now)
+	Static() bool
+}
+
+// A Type is a type for which we can emit (de)serialisation code.
+// Built in types cover the basic native types. Users can compose these into
+// custom types.
+type Type interface {
+	// Does the type support len()
+	HasLen(TypeInstance) bool
+	// MinimumSize returns the minimum serialized size of the type.
+	MinimumSize(TypeInstance) uint64
+	// The name of the type
+	Name() string
+	// PointerType returns whether the type instance is a value or a pointer to
+	// value
+	PointerType(TypeInstance) bool
+	// Read returns code to deserialise an instance of the type
+	Read(TypeInstance) string
+	// WriteSize returns code to caculate the size of an instance of the type when serialized
+	WriteSize(TypeInstance) string
+	// Write returns code to serialise an instance of the
+	Write(TypeInstance) string
+}
+
 // SetConfigFormat provides the type with a reference to the root of the
 // structure for rendering templates etc
 func (typ *ConfigType) SetConfigFormat(cf *ConfigFormat) {
