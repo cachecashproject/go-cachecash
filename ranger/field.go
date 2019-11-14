c5b7f3a76a23bb60d6b677a73f67b010c8cfb6eb
Robert Collins <robertc@cachecash.com>
2019-11-14 15:48:08 +1300

    TypeInstance adapter for non-array fields

--- ranger/field.go
+++ ranger/field.go
@@ -19,6 +19,10 @@ func (field *ConfigTypeDefinition) ConfigFormat() *ConfigFormat {
 	return field.cf
 }
 
+func (field *ConfigTypeDefinition) GetInterface() *ConfigInterface {
+	return field.cf.Types[field.ValueType].Interface
+}
+
 func (field *ConfigTypeDefinition) IsInterface() bool {
 	if field.IsNativeType() {
 		return false
@@ -26,6 +30,51 @@ func (field *ConfigTypeDefinition) IsInterface() bool {
 	return field.GetInterface() != nil
 }
 
-func (field *ConfigTypeDefinition) GetInterface() *ConfigInterface {
-	return field.cf.Types[field.ValueType].Interface
+// FieldInstance returns a TypeInstance implementation adapted to the field in
+// structure form.
+func (field *ConfigTypeDefinition) FieldInstance() TypeInstance {
+	return &FieldInstance{field: field}
+}
+
+type FieldInstance struct {
+	field *ConfigTypeDefinition
+}
+
+func (instance *FieldInstance) ConfigFormat() *ConfigFormat {
+	return instance.field.ConfigFormat()
+}
+
+func (instance *FieldInstance) GetLength() uint64 {
+	return instance.field.Require.Length
+}
+
+func (instance *FieldInstance) GetMaxLength() uint64 {
+	if instance.field.Require.MaxLength > 0 {
+		return instance.field.Require.MaxLength
+	}
+	return instance.field.MaxByteRange
+}
+
+func (instance *FieldInstance) HasLen() bool {
+	return instance.field.StructureType == "array"
+}
+
+func (instance *FieldInstance) IsPointer() bool {
+	return !instance.field.Embedded
+}
+
+func (instance *FieldInstance) QualName() string {
+	return instance.field.QualName()
+}
+
+func (instance *FieldInstance) ReadSymbolName() string {
+	return instance.WriteSymbolName()
+}
+
+func (instance *FieldInstance) WriteSymbolName() string {
+	return fmt.Sprintf("obj.%s", instance.field.FieldName)
+}
+
+func (instance *FieldInstance) Static() bool {
+	return instance.field.Require.Static
 }
